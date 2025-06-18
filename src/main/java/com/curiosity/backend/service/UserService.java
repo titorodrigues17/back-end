496b3bf8d1a36d8afa7603170f1e59a533f09327
Carlos <carlosalbertosantos@uts.edu.co>
2025-06-17 22:19:43 -0500

    ADD# Se mapea servicios de user

--- src/main/java/com/curiosity/backend/service/UserService.java
+++ src/main/java/com/curiosity/backend/service/UserService.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User saveUser(User user) {
         try {String encryptedPassword = AesService.encrypt(user.getPassword());
